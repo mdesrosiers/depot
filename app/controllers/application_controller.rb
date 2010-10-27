41589f87ef2fd1959075deefff38a89a3ef49ed5
Martin Desrosiers <mdesrosiers@gmail.com>
2010-10-26 23:13:53 -0700

    Added date and more functional tests.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_current_time
+
+  def set_current_time
+    @current_time = Time.now
+  end
+
 end
--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -15,10 +15,13 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select 'title', 'Pragprog Books Online Store'
     assert_select '#columns #side a', :minimum => 4
-    #assert_select '#main .entry', 3
-    #assert_select 'h3', 'Programming Ruby 1.9'
-    #assert_select '.price', /\$[,\d]+\.\d\d/
+    assert_select '#main #product_list table tr' do |row|
+      row.each do |element|
+        assert_select element, 'td', 3
+      end
+    end
   end
 
   test "should get new" do
