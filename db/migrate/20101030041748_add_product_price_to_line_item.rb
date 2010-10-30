9a607be33302b4fc8b5979579fcb31d03a7d4cb5
Martin Desrosiers <mdesrosiers@gmail.com>
2010-10-29 23:09:13 -0700

    Made the cart smarter.

---
+++ db/migrate/20101030041748_add_product_price_to_line_item.rb
@@ -0,0 +1,16 @@
+class AddProductPriceToLineItem < ActiveRecord::Migration
+  def self.up
+    add_column :line_items, :price, :decimal
+    LineItem.reset_column_information
+
+    say_with_time "Updating prices..." do
+      LineItem.find(:all).each do |lineitem|
+        lineitem.update_attribute :price, lineitem.product.price
+      end
+    end
+  end
+
+  def self.down
+    remove_column :line_items, :price
+  end
+end
