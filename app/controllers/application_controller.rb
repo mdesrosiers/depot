99c0500e3c03e6d982e472a63d5b2a723f7daea0
Martin Desrosiers <mdesrosiers@gmail.com>
2010-11-19 00:23:58 -0800

    Localized store

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :set_i18n_locale_from_params
   before_filter :authorize
   before_filter :set_current_time
 
@@ -14,6 +15,23 @@ class ApplicationController < ActionController::Base
     unless User.find_by_id(session[:user_id])
       redirect_to login_url, :notice => "Please log in"
     end
+  end
+
+  def set_i18n_locale_from_params
+    if params[:locale]
+      if I18n.available_locales.include?(params[:locale].to_sym)
+        I18n.locale = params[:locale]
+      else
+        flash.now[:notice] = 
+        "#{params[:locale]} translation not available"
+        logger.error flash.now[:notice]
+      end
+    end
+
+  end
+
+  def default_url_options
+    { :locale => I18n.locale }
   end
 
   private
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,12 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Depot
   class Application < Rails::Application
+
+    LANGUAGES = [
+      ['English',                  'en'],
+      ["Espa&ntilde;ol".html_safe, 'es']
+    ]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
