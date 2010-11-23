e6255bf458e75f7581bf7dae1c47a2c7be086d82
Martin Desrosiers <mdesrosiers@gmail.com>
2010-11-22 19:56:18 -0800

    Moved action mailer delivery method config to environment.rb.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,6 @@ Depot::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.delivery_method = :test
-  
   config.logger = Logger.new(config.paths.log.first, 'daily')
+
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,5 +33,4 @@ Depot::Application.configure do
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
 
-  config.action_mailer.delivery_method = :test
 end
