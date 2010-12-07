146473072e71c6d456391fde716c4ab609383bf8
Martin Desrosiers <mdesrosiers@gmail.com>
2010-12-07 00:39:08 -0800

    Don't bundle development and test gems with Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -59,5 +59,5 @@ end
 after "deploy:update_code", :bundle_install
 desc "install the necessary prerequisites"
 task :bundle_install, :roles => :app do
-  run "cd #{release_path} && bundle install"
+  run "cd #{release_path} && bundle install --without development test"
 end
